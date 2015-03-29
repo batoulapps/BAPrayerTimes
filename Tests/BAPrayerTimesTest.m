82bb403f0b3df21f0d7c5a6072e22543c169ffca
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2015-03-29 18:43:55 -0400

    travis config change

--- Tests/BAPrayerTimesTest.m
+++ Tests/BAPrayerTimesTest.m
@@ -265,7 +265,7 @@
     BAPrayerTimes *prayerTimes = [[BAPrayerTimes alloc] initWithDate:[NSDate date]
                                                             latitude:35.740068
                                                            longitude:-78.861207
-                                                            timeZone:[NSTimeZone timeZoneWithName:@"America/New_York"]
+                                                            timeZone:[NSTimeZone defaultTimeZone]
                                                               method:BAPrayerMethodMWL
                                                               madhab:BAPrayerMadhabShafi];
     
